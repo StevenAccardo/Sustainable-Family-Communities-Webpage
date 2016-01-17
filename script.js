55ce6089682285553221f10d9b68cf626a2aed8b
CaliKane <accardo.steven@gmail.com>
2016-01-17 11:33:49 -0800

    Third Commit: Updated design after client feedback

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+
+	//Video Switch function//
 	$('a').click(function(e) {
 		e.preventDefault();
 
 		$("#videoplayer").attr("src", $(this).attr("href"));
-	})
+	});
+
+	$('.button').hover(function() {
+		$(this).css({'background-color':'#FFF', 'color': '#3366cc'});
+	}, function() {
+		$(this).css({'background-color':'#3366cc', 'color':'#FFF'});
+	}
+	);
+
 });
