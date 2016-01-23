8b378dfb98b21484f01e39d83141f01041d5c545
CaliKane <accardo.steven@gmail.com>
2016-01-23 02:09:31 -0800

    Updated webpage to reflect client requests

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	//Video Switch function//
-	$('a').click(function(e) {
+	$('.row a').click(function(e) {
 		e.preventDefault();
 
 		$("#videoplayer").attr("src", $(this).attr("href"));
